e1357d8ae42422352c1121c6314b92d5156c6460
Guillaume Sere <guillaumesere@hotmail.fr>
2025-09-15 14:27:38 +0200

    css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './styles.css';
 import { initRubiksApp } from './components/RubiksCube.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('app');
+  const container = document.getElementById('main-content');
   initRubiksApp(container);
 });
